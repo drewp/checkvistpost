07ad31bcc1c1009de4ab8c6d76281afc0a6584ca
drewp <drewp@bigasterisk.com>
2013-07-06 21:50:34 -0700

    seems to work
    Ignore-this: 474935612634a726f84c9173ef48157f
    
    darcs-hash:20130707045034-312f9-eb17b2e646161b02db70c8706accb4a4bcf35f53

--- checkvistpost.py
+++ checkvistpost.py
@@ -1,16 +1,47 @@
-import restkit, json, logging
+#!bin/python
+import json, logging, email, urllib
+import klein
+from twisted.web.client import getPage
+from twisted.internet.defer import inlineCallbacks
 logging.basicConfig(level=logging.WARN)
-config = json.load(open("config.json"))
-cv = restkit.Resource("http://checkvist.com/")
-token = json.loads(cv.post(
-    "auth/login.json",
-    username=config['username'],
-    remote_key=config['remote_key'],
-  ).body_string())
-print "token %r" % token
 
-cv.post("checklists/%s/tasks.json" % config['checklist_id'], **{
-    'token': token,
-    'task[content]': 'demo1',
-    'task[position]': 1,
-})
+CHECKVIST = "http://checkvist.com"
+
+config = json.load(open("config.json"))
+
+def tasksUri():
+    return CHECKVIST + "/checklists/%s/tasks.json" % str(config['checklist_id'])
+
+@inlineCallbacks
+def postTask(taskText):
+    result = yield getPage(CHECKVIST + "/auth/login.json", method="POST",
+                postdata=urllib.urlencode({
+                    'username': config['username'],
+                    'remote_key': config['remote_key'],
+                    }))
+    token = json.loads(result)
+
+    yield getPage(tasksUri(), method="POST",
+                postdata=urllib.urlencode({
+                    'token': token,
+                    'task[content]': taskText,
+                    'task[position]': 1,
+                }))
+
+@klein.route('/')
+def index(request):
+    return ('POST an email message to <a href="/newmail">newmail</a> '
+            'to add a task. The email subject line becomes the task text.')
+    
+@klein.route('/newmail', methods=['POST'])
+def newmail(request):
+    msg = email.message_from_file(request.content)
+    subject = msg['subject']
+    if subject is None:
+        raise ValueError("no subject found")
+    
+    done = postTask(subject)
+    done.addCallback(lambda result: "ok")
+    return done
+
+klein.run('0.0.0.0', port=9108)
